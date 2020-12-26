69432a679aa3ed211a81863f3832a539140461d0
teoyi <yipengteo@yahoo.com.sg>
2020-12-25 18:45:47 -0800

    Added feature of replacing equation line every button pressed

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,11 @@ function multiply(a, b){
 };
 
 function divide(a, b){
+    if (b == 0) {
+        return "Undefined";
+    } else {
     return a / b
+    }
 }
 
 // Operation Function 
@@ -25,10 +29,35 @@ function operate(operator, a, b){
     } else if (operator === 'multiply'){
         return multiply(a,b);
     } else if (operator === 'divide'){
-        if (b == 0){
-            return Nan;
-        } else {
-            return divide(a,b);
-        }
+        return divide(a,b);
     };
 };
+
+const result = document.querySelector('#result');
+const equation = document.querySelector('#equation');
+const ops = document.querySelectorAll('.op')
+const digits = document.querySelectorAll('.digit');
+
+let eqn = '';
+let displayNum = 0;
+digits.forEach(digit => {
+    digit.addEventListener('click', function(){
+        eqn += digit.textContent;
+        document.getElementById('equation').innerHTML = '';
+        document.getElementById('equation').innerHTML += eqn;
+        console.log(eqn);
+        return eqn;
+        
+    });
+});
+ops.forEach(op => {
+    op.addEventListener('click', function(){
+        eqn += op.textContent;
+        document.getElementById('equation').innerHTML = '';
+        document.getElementById('equation').innerHTML += eqn;
+        console.log(eqn);
+        return eqn;
+        
+    });
+});
+
